29bc5331ee5de47a6b97c7e9b5842c62ea16e626
MeeLock <zalipatr1@yandex.ru>
2019-10-25 15:32:34 +0300

    added test page for dev

---
+++ test.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include "elems/head.php"?>
+</head>
+<body>
+	<style>
+		body { background: white; }
+	</style>
+<?php
+include "scripts/PHPFunctions.php";
+
+$time = "2:32.52";
+
+$sec = TimeToSec($time);
+$newTime = SecToTime($sec);
+echo "time = sec = new time<br>";
+echo "$time = $sec = $newTime";
+
+
+
+
+?>
+</body>
+</html>
